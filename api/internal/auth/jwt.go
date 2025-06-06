b4557416b137a61168f175dad95fcdbd2ae82365
Ivan Paghubasan <ivan.paghubasan@hoolisoftware.com>
2025-06-06 21:03:52 +0800

    Add auth pkg with jwt manager

---
+++ api/internal/auth/jwt.go
@@ -0,0 +1,41 @@
+package auth
+
+import (
+	"errors"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type JWTManager struct {
+	secretKey string
+}
+
+type Claims struct {
+	UserID int `json:"user_id"`
+	jwt.RegisteredClaims
+}
+
+func NewJWTManager(secret string) *JWTManager {
+	return &JWTManager{secretKey: secret}
+}
+
+func (j *JWTManager) GenerateToken(userID int) (string, error) {
+	claims := &Claims{UserID: userID, RegisteredClaims: jwt.RegisteredClaims{
+		ExpiresAt: jwt.NewNumericDate(time.Now().Add(24 * time.Hour)),
+		IssuedAt:  jwt.NewNumericDate(time.Now()),
+	}}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodES256, claims)
+	return token.SignedString([]byte(j.secretKey))
+}
+
+func (j *JWTManager) ParseToken(tokenStr string) (*Claims, error) {
+	token, err := jwt.ParseWithClaims(tokenStr, &Claims{}, func(t *jwt.Token) (interface{}, error) {
+		return []byte(j.secretKey), nil
+	})
+	if err != nil || !token.Valid {
+		return nil, errors.New("invalid token")
+	}
+	return token.Claims.(*Claims), nil
+}
